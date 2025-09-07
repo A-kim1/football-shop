0b4dde5c3d0bb9e8bf957f47166037bd80ab3a06
Amar Hakim <amarhakimhamzah@gmail.com>
2025-09-07 16:53:36 +0700

    Tugas 2 [0]

---
+++ main/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    # Ngikut yg dari tutorial 0 aja
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+
+    # ngembaliin nama productnya
+    def __str__(self):
+        return self.name
